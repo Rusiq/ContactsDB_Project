579756fe54a8ff440f3d1ee8853e8c873cfc2c70
Ruslan <ruslan@letmecode.org>
2016-08-01 15:29:39 +0300

    added import only contacts with phone number

--- app/src/main/java/com/example/ruslan/contactsdb_project/ui/activities/ImportActivity.java
+++ app/src/main/java/com/example/ruslan/contactsdb_project/ui/activities/ImportActivity.java
@@ -1,10 +1,15 @@
 package com.example.ruslan.contactsdb_project.ui.activities;
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.content.ContentResolver;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -31,6 +36,9 @@ public class ImportActivity extends AppCompatActivity implements View.OnClickLis
     private LinearLayoutManager layoutManager;
     private final DBHandler db = new DBHandler(this);
     private ArrayList<Contact> mContactArrayList = new ArrayList<>();
+    private String[] projection;
+    private Uri mContactUri;
+    private SimpleCursorAdapter mCursorAdapter;
 
 
     @Override
@@ -59,132 +67,55 @@ public class ImportActivity extends AppCompatActivity implements View.OnClickLis
         rvImport.setHasFixedSize(true);
 
 
-     /*   Cursor cursor = contentResolver.query(ContactsContract.Data.CONTENT_URI, other_query_params_for_filtering);
-
-        int indexGivenName = cursor.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.StructuredName.GIVEN_NAME);
-        int indexFamilyName = cursor.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.StructuredName.FAMILY_NAME);
-        int indexDisplayName = cursor.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME);
-
-        while (cursor.moveToNext()) {
-            String given = cursor.getString(indexGivenName);
-            String family = cursor.getString(indexFamilyName);
-            String display = cursor.getString(indexDisplayName);*/
-
-    /*    int hasPhone = 1;
-
-        Cursor contactsCursor = getContentResolver().query(Uri.parse("content://icc/adn"), null, ContactsContract.Data.HAS_PHONE_NUMBER + "=?" + " AND "
-                + ContactsContract.Data.MIMETYPE + "='" + ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE + "'", new String[]{String.valueOf(hasPhone)}, null);*/
-
-
-        //   Cursor contactsCursor = getContentResolver().query(ContactsContract.RawContacts.CONTENT_URI, null, null, null, null);
-        //    Cursor contactsCursor = getContentResolver().query();
-
-       /* Cursor c = getContentResolver().query(ContactsContract.Data.CONTENT_URI,
-                new String[] {ContactsContract.Data._ID, ContactsContract.CommonDataKinds.Phone.NUMBER, ContactsContract.CommonDataKinds.Phone.TYPE, ContactsContract.CommonDataKinds.Phone.LABEL},
-                ContactsContract.Data.RAW_CONTACT_ID + "=?" + " AND "
-                        + ContactsContract.Data.MIMETYPE + "='" + ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE + "'",
-                new String[] {String.valueOf(rawContactId)}, null);*/
-
-
-
-/*        Cursor contactsCursor = getContentResolver().query(ContactsContract.Data.CONTENT_URI, null, ContactsContract.CommonDataKinds.Phone.HAS_PHONE_NUMBER + "=?", new String[]{String.valueOf(1)}, null);
-
-
-        int indexGivenName = contactsCursor.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.StructuredName.GIVEN_NAME);
-        int indexFamilyName = contactsCursor.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.StructuredName.FAMILY_NAME);
-        //   int indexPhone = contactsCursor.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.StructuredName.HAS_PHONE_NUMBER);
-        int indexPhone = contactsCursor.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.Phone.NUMBER);
-        int indexPhone = contactsCursor.getColumnIndexOrThrow(ContactsContract.Contacts.);
-        int indexEmail = contactsCursor.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.Email.ADDRESS);
-
-        while (contactsCursor.moveToNext()) {
-
-
-            String contactFirstName = contactsCursor.getString(indexGivenName);
-            String contactLastName = contactsCursor.getString(indexFamilyName);
-            String contactPhone = contactsCursor.getString(indexPhone);
-            String contactEmail = contactsCursor.getString(indexEmail);
-
-                Contact contact = new Contact();
-                contact.setFirstName(contactFirstName);
-                contact.setLastName(contactLastName);
-                contact.setPhoneNumber(contactPhone);
-                contact.setEmail(contactEmail);
-                mContactArrayList.add(contact);
-        }*/
-
-
-        ContentResolver cr = getBaseContext().getContentResolver();
+        ContentResolver cr = getContentResolver();
         Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI,
-                        null,
-                        null,
-                        null,
-                        null);
+                null, null, null, null);
 
         if (cur.getCount() > 0) {
-            Log.i("Content provider", "Reading contact  emails");
+
             while (cur.moveToNext()) {
 
-                String contactId = cur.getString(cur.getColumnIndex(ContactsContract.Contacts._ID));
+                String id = cur.getString(cur.getColumnIndex(ContactsContract.Contacts._ID));
+                String name = cur.getString(cur.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
 
-                // Create query to use CommonDataKinds classes to fetch emails
-                Cursor phoneNumbers = cr.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
-                        null,
-                        ContactsContract.CommonDataKinds.Email.CONTACT_ID + " = " + contactId,
-                        null,
-                        null);
+                if (Integer.parseInt(cur.getString(cur.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))) > 0) {
 
+                    Cursor pCur = cr.query(
+                            ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                            null,
+                            ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
+                            new String[]{id}, null);
 
-                    /*        //You can use all columns defined for ContactsContract.Data
-                            // Query to get phone numbers by directly call data table column
+                    while (pCur.moveToNext()) {
+                        String phoneNo = pCur.getString(pCur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                        //  Toast.makeText(ImportActivity.this, "Name: " + name + ", Phone No: " + phoneNo, Toast.LENGTH_SHORT).show();
 
-                            Cursor c = getContentResolver().query(ContactsContract.Data.CONTENT_URI,
-                                      new String[] {ContactsContract.Data._ID, ContactsContract.CommonDataKinds.Phone.NUMBER, ContactsContract.CommonDataKinds.Phone.TYPE, ContactsContract.CommonDataKinds.Phone.LABEL},
-                                      ContactsContract.Data.CONTACT_ID + "=?" + " AND "
-                                              + ContactsContract.Data.MIMETYPE + "= + Phone.CONTENT_ITEM_TYPE + ",
-                                      new String[] {String.valueOf(contactId)}, null);*/
-
-          //      int indexGivenName = contactsCursor.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.StructuredName.GIVEN_NAME);
-          //      int indexFamilyName = contactsCursor.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.StructuredName.FAMILY_NAME);
-
-
-                while (phoneNumbers.moveToNext()) {
-
-                    // This would allow you get several email addresses
-                    String phoneNumbersString = cur.getString(phoneNumbers.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DATA));
-
-                    String firstNameString = cur.getString(cur.getColumnIndex(ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME));
-                    String lasttNameString = cur.getString(cur.getColumnIndex(ContactsContract.CommonDataKinds.StructuredName.FAMILY_NAME));
-
-                    //Log.e("email==>", emailAddress);
-                    Contact contact = new Contact();
-                    contact.setPhoneNumber(phoneNumbersString);
-                    contact.setFirstName(firstNameString);
-                    contact.setLastName(lasttNameString);
-                    mContactArrayList.add(contact);
-
+                        Contact contact = new Contact();
+                        contact.setFirstName(name);
+                        contact.setPhoneNumber(phoneNo);
+                        mContactArrayList.add(contact);
+                    }
+                    pCur.close();
                 }
-                phoneNumbers.close();
             }
-
         }
-        /*else
-        {
-            emaildata +="
-            Data not found.
-            ";
 
-        }*/
-        cur.close();
-
-
-
-
-
-
-
+        listAccounts(this);
 
     }
+
+    public void listAccounts(Context ctx) {
+
+        AccountManager am = AccountManager.get(ctx);
+        Account[] accounts = am.getAccounts();
+
+        for (Account ac: accounts){
+            String acname=ac.name;
+            String actype=ac.type;
+            Log.d("accountinfo", acname + " : " + actype);
+        }
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -267,6 +198,8 @@ public class ImportActivity extends AppCompatActivity implements View.OnClickLis
     public void onItemClick(int position) {
 
     }
+
+
 }
 
 
@@ -293,3 +226,156 @@ while (contactsCursor.moveToNext()) {
         contact.setFirstName(contactName);
         mContactArrayList.add(contact);
         }*/
+
+
+
+
+
+
+
+
+
+
+
+  /*   Cursor cursor = contentResolver.query(ContactsContract.Data.CONTENT_URI, other_query_params_for_filtering);
+
+        int indexGivenName = cursor.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.StructuredName.GIVEN_NAME);
+        int indexFamilyName = cursor.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.StructuredName.FAMILY_NAME);
+        int indexDisplayName = cursor.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME);
+
+        while (cursor.moveToNext()) {
+            String given = cursor.getString(indexGivenName);
+            String family = cursor.getString(indexFamilyName);
+            String display = cursor.getString(indexDisplayName);*/
+
+    /*    int hasPhone = 1;
+
+        Cursor contactsCursor = getContentResolver().query(Uri.parse("content://icc/adn"), null, ContactsContract.Data.HAS_PHONE_NUMBER + "=?" + " AND "
+                + ContactsContract.Data.MIMETYPE + "='" + ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE + "'", new String[]{String.valueOf(hasPhone)}, null);*/
+
+
+//   Cursor contactsCursor = getContentResolver().query(ContactsContract.RawContacts.CONTENT_URI, null, null, null, null);
+//    Cursor contactsCursor = getContentResolver().query();
+
+       /* Cursor c = getContentResolver().query(ContactsContract.Data.CONTENT_URI,
+                new String[] {ContactsContract.Data._ID, ContactsContract.CommonDataKinds.Phone.NUMBER, ContactsContract.CommonDataKinds.Phone.TYPE, ContactsContract.CommonDataKinds.Phone.LABEL},
+                ContactsContract.Data.RAW_CONTACT_ID + "=?" + " AND "
+                        + ContactsContract.Data.MIMETYPE + "='" + ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE + "'",
+                new String[] {String.valueOf(rawContactId)}, null);*/
+
+
+
+/*        Cursor contactsCursor = getContentResolver().query(ContactsContract.Data.CONTENT_URI, null, ContactsContract.CommonDataKinds.Phone.HAS_PHONE_NUMBER + "=?", new String[]{String.valueOf(1)}, null);
+
+
+        int indexGivenName = contactsCursor.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.StructuredName.GIVEN_NAME);
+        int indexFamilyName = contactsCursor.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.StructuredName.FAMILY_NAME);
+        //   int indexPhone = contactsCursor.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.StructuredName.HAS_PHONE_NUMBER);
+        int indexPhone = contactsCursor.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.Phone.NUMBER);
+        int indexPhone = contactsCursor.getColumnIndexOrThrow(ContactsContract.Contacts.);
+        int indexEmail = contactsCursor.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.Email.ADDRESS);
+
+        while (contactsCursor.moveToNext()) {
+
+
+            String contactFirstName = contactsCursor.getString(indexGivenName);
+            String contactLastName = contactsCursor.getString(indexFamilyName);
+            String contactPhone = contactsCursor.getString(indexPhone);
+            String contactEmail = contactsCursor.getString(indexEmail);
+
+                Contact contact = new Contact();
+                contact.setFirstName(contactFirstName);
+                contact.setLastName(contactLastName);
+                contact.setPhoneNumber(contactPhone);
+                contact.setEmail(contactEmail);
+                mContactArrayList.add(contact);
+        }*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+    ContentResolver cr = getBaseContext().getContentResolver();
+    Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI,
+            null,
+            null,
+            null,
+            null);
+
+if (cur.getCount() > 0) {
+        Log.i("Content provider", "Reading contact  emails");
+        while (cur.moveToNext()) {
+
+        String contactId = cur.getString(cur.getColumnIndex(ContactsContract.Contacts._ID));
+
+        // Create query to use CommonDataKinds classes to fetch emails
+        Cursor phoneNumbers = cr.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+        null,
+        ContactsContract.CommonDataKinds.Email.CONTACT_ID + " = " + contactId,
+        null,
+        null);
+
+
+        //You can use all columns defined for ContactsContract.Data
+                            // Query to get phone numbers by directly call data table column
+
+                            Cursor c = getContentResolver().query(ContactsContract.Data.CONTENT_URI,
+                                      new String[] {ContactsContract.Data._ID, ContactsContract.CommonDataKinds.Phone.NUMBER, ContactsContract.CommonDataKinds.Phone.TYPE, ContactsContract.CommonDataKinds.Phone.LABEL},
+                                      ContactsContract.Data.CONTACT_ID + "=?" + " AND "
+                                              + ContactsContract.Data.MIMETYPE + "= + Phone.CONTENT_ITEM_TYPE + ",
+                                      new String[] {String.valueOf(contactId)}, null);
+
+
+        //      int indexGivenName = contactsCursor.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.StructuredName.GIVEN_NAME);
+        //      int indexFamilyName = contactsCursor.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.StructuredName.FAMILY_NAME);
+
+
+        while (phoneNumbers.moveToNext()) {
+
+        // This would allow you get several email addresses
+        String phoneNumbersString = cur.getString(phoneNumbers.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DATA));
+
+        //  String firstNameString = cur.getString(cur.getColumnIndex(ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME));
+        //  String lasttNameString = cur.getString(cur.getColumnIndex(ContactsContract.CommonDataKinds.StructuredName.FAMILY_NAME));
+
+        //Log.e("email==>", emailAddress);
+        Contact contact = new Contact();
+        contact.setPhoneNumber(phoneNumbersString);
+        //    contact.setFirstName(firstNameString);
+        //    contact.setLastName(lasttNameString);
+        mContactArrayList.add(contact);
+
+        }
+        phoneNumbers.close();
+        }
+
+        }
+else
+        {
+            emaildata +="
+            Data not found.
+            ";
+
+        }
+
+        cur.close();
+
+
+*/
+
+
+
+
+
